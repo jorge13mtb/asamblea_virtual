120e658c82f3d671f6dfc554cb2af77e5b00e2fd
jorge13mtb <jorge13mtb@gmail.com>
2014-05-17 15:34:59 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsambleaVirtual::Application.config.secret_key_base = '0b5c5cd5df5b70cfcc8474b34e0badf4b4b6dc0ee1eabe08bb82df9996ad3fb2757423c62b6d50362340c77e5dca12ab2a2115a5d2b9294669d4717e0e96d15f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AsambleaVirtual::Application.config.session_store :cookie_store, key: '_asamblea_virtual_session'
